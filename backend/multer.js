8dc6b9f53602d98b0c598da50b8fe96839ae73e4
sam22kalvium <samragyisharma@kalvium.community>
2025-01-25 15:13:33 +0530

    Milestone 4 done

--- backend/multer.js
+++ backend/multer.js
@@ -3,10 +3,10 @@ const multer = require('multer');
 // Configure multer storage
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'public/uploads'); //Define your upload folder
+    cb(null, 'uploads/'); //Define your upload folder
   },
   filename: function (req,file,cb){
-    const uniqueSuffix = Date.now() + '-' + Math.roundapply(Math.random() * 1e9);
+    const uniqueSuffix = Date.now() + '-' + Math.round.apply(Math.random() * 1e9);
     //Define a unique filename
     const filename = file.originalname.split('.')[0];   
     cb(null, filename + '-' + uniqueSuffix + ".png" );
